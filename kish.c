642dc924599a22f78e9e6b24a42740de422b0bf7
david.ferrari <david.ferrari@primeurad.com>
2024-06-27 18:06:41 +0200

    First version: single command executed with fork/exec

--- kish.c
+++ kish.c
@@ -2,23 +2,41 @@
 
 #include<errno.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<spawn.h>
 #include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
 int main() {
 	while(1) {
 		printf("--kish--$ ");
 		char *prompt = NULL;
 		size_t n = 0;
-		errno=2;
 		if (getline(&prompt, &n, stdin) == -1) {
 			if(!feof(stdin)) {
 				printf("%s\n", strerror(errno));
 			}
 			break;
 		}
-		// Here prompt contains the entire command 
-
+		prompt[strlen(prompt) - 1] = 0;
+		if (strlen(prompt) > 0) {
+			pid_t cpid = fork();
+			switch(cpid) {
+				case -1:
+					perror("fork");
+					exit(EXIT_FAILURE);
+				case 0:
+					execlp(prompt, prompt, NULL);
+					fprintf(stderr,"%s\n", strerror(errno));
+					exit(EXIT_FAILURE);
+				default:
+					wait(NULL);
+					break;
+			}
+		}
 
 	}
 	putchar('\n');
+	return EXIT_SUCCESS;
 }
